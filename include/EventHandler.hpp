5b4cf1b3bdad8977df4810f3640dd01bb1298b7f
Ryan Ebrahimi <ryan_ebrahimi@yahoo.com>
2021-06-26 13:58:38 -0400

    Add EventHandler.hpp, most likely temporary

---
+++ include/EventHandler.hpp
@@ -0,0 +1,19 @@
+#ifndef GAME_EVENTHANDLER
+#define GAME_EVENTHANDLER
+
+struct Params {
+  bool W_PRESSED = false;
+  bool A_PRESSED = false;
+  bool S_PRESSED = false;
+  bool D_PRESSED = false;
+
+  bool UP_PRESSED;
+  bool LEFT_PRESSED;
+  bool DOWN_PRESSED;
+  bool RIGHT_PRESSED;
+};
+
+class EventHandler {
+
+};
+#endif
