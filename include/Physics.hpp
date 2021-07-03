db1bf8396f692880efda3183c080c468a6fdd4c3
Ryan Ebrahimi <ryan_ebrahimi@yahoo.com>
2021-07-03 18:08:29 -0400

    Acceleration physics in progress, bug: values start at random places

--- include/Physics.hpp
+++ include/Physics.hpp
@@ -22,10 +22,33 @@ class PhysicsSim {
         struct Rigidbody::RigidbodyData* rb=(struct Rigidbody::RigidbodyData*)a->GetComponent(RIGIDBODY)->GetData();
         struct Transform::TransformData* transform=(struct Transform::TransformData*)a->GetComponent(TRANSFORM)->GetData();
 
-        if (params->W_PRESSED) transform->posY += 1;
-        if (params->S_PRESSED) transform->posY -= 1;
-        if (params->D_PRESSED) transform->posX += 1;
-        if (params->A_PRESSED) transform->posX -= 1;
+        float carForceMax = 0.001;
+        float carTorqueMax = 0.001;
+
+        float newForce = 0;
+        float newTorque = 0;
+
+        if (params->W_PRESSED) newForce += carForceMax;
+        if (params->S_PRESSED) newForce -= carForceMax;
+        if (params->D_PRESSED) newTorque += carTorqueMax;
+        if (params->A_PRESSED) newTorque -= carTorqueMax;
+
+        rb->force = newForce;
+        rb->torque = newTorque;
+
+        rb->accelerationX = sin(transform->rotation) * rb->force;
+        rb->accelerationY = cos(transform->rotation) * rb->force;
+
+        rb->velocityX += rb->accelerationX;
+        rb->velocityY += rb->accelerationY;
+
+        transform->posX += rb->velocityX;
+        transform->posY += rb->velocityY; //TODO something is not being initialized properly and is causing the car to start way off the map
+
+        // rb->angularAcceleration = rb->torque;
+        // rb->angulaaaaaaaaaaab rVelocity += rb->angularAcceleration;
+
+        //transform->rotation += rb->angularVelocity;
       }
     }
 };
