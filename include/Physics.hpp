0815c68ab521d5dac5c88b988b450d26db9265fd
Ryan Ebrahimi <ryan_ebrahimi@yahoo.com>
2021-08-14 18:22:10 -0400

    Modify physics system

--- include/Physics.hpp
+++ include/Physics.hpp
@@ -21,36 +21,12 @@ class PhysicsSim {
       for(Entity* a : entities) {
         struct Rigidbody::RigidbodyData* rb=(struct Rigidbody::RigidbodyData*)a->GetComponent(RIGIDBODY)->GetData();
         struct Transform::TransformData* transform=(struct Transform::TransformData*)a->GetComponent(TRANSFORM)->GetData();
-
-        float carForceMax = 0.01;
-        float carTorqueMax = 0.01;
-
-        float newForce = 0;
-        float newTorque = 0;
-
+        
         float dirX = sin(transform->rotation * M_PI / 180);
         float dirY = cos(transform->rotation * M_PI / 180);
 
-        std::cout << "Direction: (" << dirX << ", " << dirY << ")\n";
-
-        if (params->W_PRESSED) newForce += carForceMax;
-        if (params->S_PRESSED) newForce -= carForceMax;
-        //if (params->D_PRESSED) newTorque += carTorqueMax;
-        //if (params->A_PRESSED) newTorque -= carTorqueMax;
-
-        if (params->D_PRESSED) rb->angularVelocity = 1;
-        if (params->A_PRESSED) rb->angularVelocity = -1;
-
-        if (!params->D_PRESSED && !params->A_PRESSED) {
-          rb->angularVelocity = 0;
-        }
-
-        // position calcs
-        rb->force = newForce;
-
         rb->accelerationX = rb->force * dirX;
         rb->accelerationY = rb->force * dirY;
-
         rb->velocityX += rb->accelerationX;
         rb->velocityY += rb->accelerationY;
 
@@ -58,18 +34,14 @@ class PhysicsSim {
 
         rb->velocityX = speed * dirX;
         rb->velocityY = speed * dirY;
-
-        std::cout << "Velocity: (" << rb->velocityX << ", " << rb->velocityY << ") - Magnitude: " << sqrt(rb->velocityX * rb->velocityX + rb->velocityY * rb->velocityY) << std::endl;
-
         transform->posX += rb->velocityX;
         transform->posY += rb->velocityY;
 
-        // rotation calcs
-        rb->torque = newTorque;
+        std::cout << "Direction: (" << dirX << ", " << dirY << ")\n";
+        std::cout << "Velocity: (" << rb->velocityX << ", " << rb->velocityY << ") - Magnitude: " << sqrt(rb->velocityX * rb->velocityX + rb->velocityY * rb->velocityY) << std::endl;
 
         //rb->angularAcceleration = rb->torque;
         //rb->angularVelocity += rb->angularAcceleration;
-
         transform->rotation += rb->angularVelocity;
       }
     }
