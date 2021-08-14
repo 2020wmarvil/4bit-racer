9dbc253d595927115761f8ef9e2bbca78e9f2614
Ryan Ebrahimi <ryan_ebrahimi@yahoo.com>
2021-08-14 18:22:20 -0400

    Subpixel precision

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -1,6 +1,10 @@
 #ifndef GAME_RENDERER
 #define GAME_RENDERER
 
+#ifndef SCREEN_SIZE
+#define SCREEN_SIZE 480
+#endif
+
 #include <vector>
 #include <SDL2/SDL.h>
 
@@ -30,15 +34,17 @@ class Renderer {
           struct Texture::TexData* texdata=(struct Texture::TexData*)a->GetComponent(TEXTURE)->GetData();
           struct Transform::TransformData* transformdata=(struct Transform::TransformData*)a->GetComponent(TRANSFORM)->GetData();
           if(texdata->layer==i) {
-            SDL_Rect rect;
-            SDL_QueryTexture(texdata->tex,NULL,NULL,&rect.w,&rect.h);
+            SDL_FRect rect;
+            int wid,hei;
+            SDL_QueryTexture(texdata->tex,NULL,NULL,&wid,&hei);
+            rect.w=wid;rect.h=hei;
             rect.w*=texdata->scale;
             rect.h*=texdata->scale;
             rect.x=SCREEN_SIZE*(transformdata->posX+100)/200-rect.w/2;
             rect.y=SCREEN_SIZE*(transformdata->posY+100)/200+rect.h/2;
             rect.y=SCREEN_SIZE-rect.y;
             //SDL_RenderCopy(ren,texdata->tex,NULL,&rect);
-            SDL_RenderCopyEx(ren,texdata->tex,NULL,&rect,transformdata->rotation,NULL,SDL_FLIP_NONE);
+            SDL_RenderCopyExF(ren,texdata->tex,NULL,&rect,transformdata->rotation,NULL,SDL_FLIP_NONE);
             empty=false;
           }
         }
